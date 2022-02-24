86776273f35db4894abb9eb0d1556a73e74e923f
susautw <susautw@gmail.com>
2022-02-24 12:00:52 +0800

    Misc Fixes Add normalize Use size instead of scale Force images to RGB

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -5,16 +5,19 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import torchvision
 from PIL import Image
 from torch.utils.data import Dataset
 
 
+normalize = torchvision.transforms.Normalize((0.5,) * 3, (0.5,) * 3)
+
+
 class BasicDataset(Dataset):
-    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+    def __init__(self, images_dir: str, masks_dir: str, size: int, mask_suffix: str = '_mask'):
         self.images_dir = Path(images_dir)
         self.masks_dir = Path(masks_dir)
-        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
-        self.scale = scale
+        self.size = size
         self.mask_suffix = mask_suffix
 
         self.ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
@@ -26,11 +29,10 @@ class BasicDataset(Dataset):
         return len(self.ids)
 
     @classmethod
-    def preprocess(cls, pil_img, scale, is_mask):
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+    def preprocess(cls, pil_img, size, is_mask):
+        if not is_mask:
+            pil_img = pil_img.convert("RGB")
+        pil_img = pil_img.resize((size, size), resample=Image.NEAREST if is_mask else Image.BICUBIC)
         img_ndarray = np.asarray(pil_img)
 
         if img_ndarray.ndim == 2 and not is_mask:
@@ -40,6 +42,8 @@ class BasicDataset(Dataset):
 
         if not is_mask:
             img_ndarray = img_ndarray / 255
+        else:
+            img_ndarray = np.int32(img_ndarray > 0)
 
         return img_ndarray
 
@@ -66,15 +70,15 @@ class BasicDataset(Dataset):
         assert img.size == mask.size, \
             'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
 
-        img = self.preprocess(img, self.scale, is_mask=False)
-        mask = self.preprocess(mask, self.scale, is_mask=True)
+        img = self.preprocess(img, self.size, is_mask=False)
+        mask = self.preprocess(mask, self.size, is_mask=True)
 
         return {
-            'image': torch.as_tensor(img.copy()).float().contiguous(),
+            'image': normalize(torch.as_tensor(img.copy()).float().contiguous()),
             'mask': torch.as_tensor(mask.copy()).long().contiguous()
         }
 
 
 class CarvanaDataset(BasicDataset):
-    def __init__(self, images_dir, masks_dir, scale=1):
-        super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
+    def __init__(self, images_dir, masks_dir, size=256):
+        super().__init__(images_dir, masks_dir, size, mask_suffix='_mask')
